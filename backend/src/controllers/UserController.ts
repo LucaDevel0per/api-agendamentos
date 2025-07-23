7e8863d1abb2f662c28a7a10bba55689a8be6f12
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-07-23 12:26:10 -0300

    feat: added data validation with ZOD lib

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
+import { z } from 'zod';
 import CreateUserService from "../services/CreateUserService";
+import { Role } from "../../generated/prisma";
+
+
+const createUserBodySchema = z.object({
+    name: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(6),
+    role: z.enum([Role.USER, Role.PROVIDER, Role.ADMIN]).optional()
+})
 
 class UserController {
     public async create(req: Request, res: Response): Promise<Response> {
+
+        const { name, email, password, role } = createUserBodySchema.parse(req.body);
         const createUser = new CreateUserService
-        const { name, email, password, role } = req.body
         const user = await createUser.execute({ name, email, password, role })
 
         return res.status(201).json(user);
