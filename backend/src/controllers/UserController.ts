c89e3a6ef166c21a2a6655b89ae96df0f38113d6
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-07-24 11:57:26 -0300

    feat: passwords strongers

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -3,11 +3,16 @@ import { z } from 'zod';
 import CreateUserService from "../services/CreateUserService";
 import { Role } from "../../generated/prisma";
 
+const strongPasswordRegex = new RegExp(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+);
 
 const createUserBodySchema = z.object({
     name: z.string().min(3),
-    email: z.email(),
-    password: z.string().min(6),
+    email: z.email({ message: "Formato de e-mail inválido."}),
+    password: z.string().regex(strongPasswordRegex, {
+        message: "A senha precisa ter no mínimo 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caractere especial (@$!%*?&)."
+    }), 
     role: z.enum([Role.USER, Role.PROVIDER, Role.ADMIN]).optional()
 })
 
